13765e5476551c6091dd734c7932a1ca62879e3c
sebanti <sebanc99@gmail.com>
2020-07-02 19:01:32 +0530

    Update Dicitonary

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -1,51 +1,64 @@
 import PyPDF2
 import re
 
-pdfFileObj = open('Arihant-general_knowledge.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
 
-pages = pdfReader.numPages
-final_dict={}
-qno=1
+def extract_questions(total_pages):
+	pdfFileObj = open('Arihant-general_knowledge.pdf', 'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
 
-for page in range(pages):
-	firstPage = pdfReader.getPage(page) 
-	fp_text=firstPage.extractText()
+	#pages = pdfReader.numPages
+	final_dict={}
+	qno=1
+	c=1
 
-
-	quizRegex = re.compile(r'\n\d.\n|\n\d\d.\n')
-	mo=quizRegex.findall((fp_text))
-	questions=len(mo)
-
-	newfp=fp_text.split('\n')
-
-	for x in range(questions):
-		new1,new2=0,0
-		c=1
-		for i in range(len(newfp)):
-			if str(c)+"." == newfp[i]:
-				new1=i
-				c+=1
-				break
-
-		for i in range(new1,len(newfp)):
-			if str(c)+"." == newfp[i]:
-				new2=i
-				break
-
-		qs=newfp[new1+1:new2-1]
-		qs=''.join(qs)
-		newqs=qs.split('(1)',1)
-		newqs[1]='(1)'+newqs[1]
-		print(x, end=' ')
-		print("no data is ")
-		print(newqs)
-		print("===================")
-		final_dict[qno] = newqs
-		qno+=1
+	for page in range(total_pages):
+		firstPage = pdfReader.getPage(page) 
+		fp_text=firstPage.extractText()
 
 
+		quizRegex = re.compile(r'\n\d.\n|\n\d\d.\n')
+		mo=quizRegex.findall((fp_text))
+		questions=len(mo)
+
+		newfp=fp_text.split('\n')
+
+		for x in range(questions):
+			new1,new2=0,0
+			for i in range(len(newfp)):
+				if str(c)+"." == newfp[i]:
+					new1=i
+					c+=1
+					break
+
+			for i in range(new1,len(newfp)):
+				if str(c)+"." == newfp[i]:
+					new2=i
+					break
+
+			qs=newfp[new1+1:new2-1]
+			qs=''.join(qs)
+			newqs=qs.split('(1)',1)
+			newqs[1]='(1)'+newqs[1]
+			#print(qno, end=' ')
+			#print("no data is ")
+			#print(newqs)
+			#print("===================")
+			final_dict[qno] = newqs
+			qno+=1
+
+
+	pdfFileObj.close()
+	return final_dict
 
 
 
-pdfFileObj.close()
+if __name__=='__main__':
+	print("Enter how many pages you want questions for: ")
+	pages=int(input())
+	final_dict={}
+	final_dict=extract_questions(pages)
+
+	for i,j in final_dict.items():
+		print(i, end='. ')
+		print(j)
+		print("=============================")
